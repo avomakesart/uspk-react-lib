1eca3f98caaaf1cb69f9b340aac78400e9014a23
Alvaro <alvcastillo@paypal.com>
2021-11-06 12:38:38 -0600

    chore(fix): fixing build issues on prod

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,44 +1,24 @@
+import commonjs from '@rollup/plugin-commonjs'
 import resolve from '@rollup/plugin-node-resolve'
-import replace from '@rollup/plugin-replace'
+import peerDepsExternal from 'rollup-plugin-peer-deps-external'
 import typescript from '@rollup/plugin-typescript'
-import babel from 'rollup-plugin-babel'
-import external from 'rollup-plugin-peer-deps-external'
 import postcss from 'rollup-plugin-postcss'
-import { terser } from 'rollup-plugin-terser'
 
-export default [
-  {
-    input: './packages/index.ts',
-    output: [
-      {
-        file: 'lib/index.js',
-        format: 'cjs',
-      },
-      {
-        file: 'lib/index.es.js',
-        format: 'es',
-        exports: 'named',
-      },
-    ],
-    plugins: [
-      replace({
-        'process.env.NODE_ENV': JSON.stringify('development'),
-        preventAssignment: true,
-      }),
-      typescript({ sourceMap: false, declaration: true, declarationMap: true }),
-      postcss({
-        plugins: [],
-        minimize: true,
-      }),
-      babel({
-        exclude: 'node_modules/**',
-        presets: ['@babel/preset-react'],
-      }),
-      external(),
-      resolve({
-        browser: true,
-      }),
-      terser(),
-    ],
-  },
-]
+import packageJson from './package.json'
+
+export default {
+  input: './packages/index.ts',
+  output: [
+    {
+      file: packageJson.main,
+      format: 'cjs',
+      sourcemap: true,
+    },
+    {
+      file: packageJson.module,
+      format: 'esm',
+      sourcemap: true,
+    },
+  ],
+  plugins: [peerDepsExternal(), resolve(), commonjs(), typescript(), postcss()],
+}
